0a1d8476892e7453283f7c1f4b66984bd5433d22
Luis03 <rage.875@gmail.com>
2019-05-31 20:02:32 -0500

    clase_10 Ejercicios

---
+++ sample-math.js
@@ -0,0 +1,6 @@
+const customMath = require('./custom-math')
+
+console.log(customMath.suma(4,2))
+console.log(customMath.resta(4,2))
+console.log(customMath.multiplicacion(4,2))
+console.log(customMath.division(4,2))
